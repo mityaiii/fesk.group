4ebbb9963de14ec637dcaee5d17a339ca9d8c86c
mityaiii <pericle04@mail.ru>
2024-01-30 18:36:16 +0300

    changed urls and models

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('products', views.ProductView.as_view(), name="products"),
     path('products/<int:pk>', views.ProductView.as_view(), name="products-by-id"),
-    path('caregories', views.CategoryView.as_view(), name="caregories-by-id"),
-    path('caregories/<int:pk>', views.CategoryView.as_view(), name="caregories-by-id"),
+    path('categories', views.CategoryView.as_view(), name="caregories-by-id"),
+    path('categories/<int:pk>', views.CategoryView.as_view(), name="caregories-by-id"),
 ]
